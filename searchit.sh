2a32858c867cc56be7de860e92ae928b20dcbbf7
jmsfkjskjf <hvfshdfkdfs>
2025-12-14 13:10:04 +0300

    added text visuals and while loop

--- searchit.sh
+++ searchit.sh
@@ -1,25 +1,41 @@
 #!/bin/bash
 
-echo -e "\n\t\t----------"
-echo -e "\t\tSEARCH IT"
-echo -e "\t\t----------"
+echo -e "
+                   _ _ _ _ _ _ _	
+		  /  _ _ _ _ _ _ \	
+                 / /		\ \	
+		/ /    _ _ _	 \ \	
+		| |   | 	 | | 	 __ 	 __	 __		     _____	
+	 __	| |   |_ _ _	 | |	|__|	|__|	|	|__|	   |   |	
+	|__	| |   |	         | |    |  |	|\	|__	|  |	   |   |	
+	 __|	\ \   |_ _ _	 / /	
+		 \ \ _ _ _ _ _ _/ /	
+		  \ _ _ __ _ _ _ \	
+				\ \	
+				 \ \	
+				  \_\	
 
-read -p "Enter Filename or Foldername to search: " filename
+								"
 
-if [ -z "$filename" ]; then
-    echo "Error: No name entered!"
-    exit 1
-fi
+while true; do
+	echo -e "\nTo stop search press CTRL+Z. \n"
 
-echo -e "\n\n Searching folders with the name $filename ....\n -----------------------------------------"
-	find / -type d -name "$filename" 2>/dev/null
+	echo -e -n "Enter Filename or Foldername to search: " 
+	read -r filename
 
-echo -e "\n\n Searching files with the name $filename ....\n ---------------------------------------"
-	find / -type f -name "$filename" 2>/dev/null
+	if [ -z "$filename" ]; then
+		echo "Error: No filename entered!"
+		exit 1
+	fi
 
-echo -e "\n\n Deep search...\n (Searching all files and folders with the name $filename)\n ----------------------------------------------------"
-	find / -name "$filename" -o -name "$filename.*" 2>/dev/null
-echo
-echo -e "Search completed!\n"
+	echo
+	echo "Searching for: $filename"
+	echo "Searching....."
+	echo
 
+	find / -iname "*$filename*" 2>/dev/null
+	#find / -name "$filename" -o -name "$filename.*" 2>/dev/null
+	echo
+	echo -e "Search completed!\n\n"
 
+done
